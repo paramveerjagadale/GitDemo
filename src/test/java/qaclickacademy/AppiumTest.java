7fe49eeed8218505b77abeb3a08d29e5de33791e
Paramveer <paramveerjagadale@gmail.com>
2021-12-27 00:03:32 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAPPAndroid()
+	{
+		System.out.println("NativeAPPAndroid");
+	}
+
+	@Test
+	public void ISOApps()
+	{
+		System.out.println("ISOApps");
+	}
+
+
+}
