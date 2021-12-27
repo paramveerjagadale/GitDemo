a1f25b4dfd5a88b265581fb9faf239fc10585d33
paramveer <paramveerjagadale@gmail.com>
2021-12-27 21:11:32 +0530

    American guy commit

--- src/test/java/qaclickacademy/RestAPITest.java
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -11,6 +11,15 @@ public class RestAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		//Updated by ASIAN timezone guy
+		
+		//Updated by USA timezone guy
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
+		System.out.println("postJira8");
+		System.out.println("postJira9");
 	}
 
 	@Test
